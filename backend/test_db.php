96d208ba8ac531da89d0e6b0fdac4ab7db97b9dd
Mahir Ahmed <mahir101748@gmail.com>
2025-10-11 00:58:47 +0600

    debugging removed

--- backend/test_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("mysql:host=localhost;dbname=ekta_tay;charset=utf8", "root", "");
-    echo "✅ DB Connected!";
-} catch (PDOException $e) {
-    echo "❌ DB Connection Failed: ".$e->getMessage();
-}
-?>
